02880322bb2523991793eedc6966be3a1f105493
britney carrier <bcarrier2010@gmail.com>
2016-03-01 08:36:33 -0800

    Started skeleton for creating user accounts

---
+++ user_account.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import subprocess
+
+def add_group():
+"""
+Adds a specific user to their own group. 
+
+Inputs:
+    user_group which is a string
+
+Returns:
+    success or failure
+
+"""
+    pass;
+
+def add_user():
+"""
+Uses pw adduser to add a user to the system
+
+Inputs:
+
+Returns:
+    success or failure
+"""
+    pass;
+
+def send_msg():
+"""
+
+"""
+    pass;
+
+def drop_keys():
+"""
+"""
+    pass
+
