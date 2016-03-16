af9e68995da1c451509083f4a785aef5942382aa
Sophie <sophieanderson80@gmail.com>
2016-03-16 12:05:15 -0700

    created the decode() function in order to deal with metadata.json specifically.

--- jailify/extract.py
+++ jailify/extract.py
@@ -6,6 +6,7 @@
 
 import os
 import sys
+import json
 import tarfile
 import zipfile
 import os.path
@@ -20,17 +21,20 @@ def main(argv):
     Returns:
         None
     """
-
+    print()
     # Check for correct number of args and get team name.
     if (len(sys.argv) == 2):
         file_name = argv[1]
+        print("file_name set to: " + file_name)
         team_name = check_title(file_name)
+        print("team_name set to: " + team_name)
     else:
         print("Incorrect number of arguments")
         sys.exit()
 
     # Determine and return the file type.
     file_type = inspect_file(file_name)
+    print("file_type is: " + file_type + " in main")
 
     # Extract based on file type
     extract(file_type, file_name)
@@ -47,6 +51,7 @@ def check_title(filename):
         teamname (str): name of the team, which is file name minus the file
                         extension
     """
+    print("command line argument sent to check_title")
     if os.path.isfile(filename) or os.path.isdir(filename):
         teamname = os.path.splitext(filename)[0]
         return teamname
@@ -64,16 +69,22 @@ def inspect_file(command_line_argument):
         file_type (str): aborts if file is not a directory, gzip, zip or xz compressed file.
                          otherwise returns a string representing one of the four types.
     """
+    print("team_name sent to inspect_file. Let's find the type.")
     if os.path.isdir(command_line_argument):
         file_type = "dir"
+        print("file_type set to: " + file_type)
     elif mimetypes.guess_type(command_line_argument)[1] == 'bzip2':
         file_type = "bz2"
+        print("file_type set to: " + file_type)
     elif mimetypes.guess_type(command_line_argument)[1] == 'gzip':
         file_type = "gz"
+        print("file_type set to: " + file_type)
     elif zipfile.is_zipfile(command_line_argument):
         file_type = "zip"
+        print("file_type set to: " + file_type)
     elif mimetypes.guess_type(command_line_argument)[1] == "xz":
         file_type = "xz"
+        print("file_type set to: " + file_type)
     else:
         print("Type is unacceptable")
         sys.exit()
@@ -95,8 +106,10 @@ def extract(filetype, filename):
     """
 
     if filetype == "bz2" or filetype == "gz" or filetype == "xz":
+        print("since file type is " + filetype + "call extract_tar")
         extract_tar(filename, filetype)
     elif filetype == "zip":
+        print("since file type is " + filetype + "call extract_tar")
         extract_zip(filename)
     elif filetype == "dir":
         print("Deal with directory")
@@ -119,16 +132,60 @@ def extract_tar(filenametar, comptype):
     Returns:
         L (list): the members as a list of TarExFile objects.
     """
+    get_metadata = False
+    print("Parsing through the contents of the archive:")
     try:
         with tarfile.open(filenametar, 'r:{}'.format(comptype)) as tar: 
             for f in tar:
                 if os.path.basename(f.name) == "metadata.json":
-                    data = tar.extractfile(f).read()
-                    print(data)
+                    decode(tar.extractfile(f))
+                    get_metadata = True
                 elif os.path.basename(f.name).endswith('.pub'):
                     print(".pub")
     except tarfile.TarError:
         print("Couldn't open tarfile")
+
+
+
+
+def decode(json_file_object):
+    """Decodes and extracts contents of the metadata.json file.
+
+    Args:
+        json_file_object (???): the extracted metadata.json file object.
+    Returns:
+        None
+    """
+    print("JSON file object sent to decode()")
+    try:
+        json_contents_in_dictionary = json.JSONDecoder(json_file_object)
+        print("json contents:")
+        print(json_contents_in_dictionary)
+    except json.JSONDecodeError():
+        print("Unable to decode json file object")
+        sys.exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def extract_zip(zipfilename):
