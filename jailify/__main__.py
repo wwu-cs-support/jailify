58d97030db6dc446a372b16e48d75ba31b3c48ef
britney carrier <bcarrier2010@gmail.com>
2016-03-17 13:47:37 -0700

    Added handling command line args to __main__.py

--- jailify/__main__.py
+++ jailify/__main__.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import click
 import functools
 
 import jailify.util
@@ -19,11 +20,29 @@ def root_check(func):
             func(*args, **kwargs)
     return _wrapper
 
+@click.command()
+@click.argument('jail_directory', type=click.Path(exists=True, readable=True))
+@root_check
+def jailify_main(jail_directory):
+    print("In jailify main and this argument thing is working {}".format(jail_directory))
+
 
 @root_check
-def jailify_main():
-    print("In jailify main")
+@click.command()
+@click.argument('jail_name', required=False)
+def dejailify_main(jail_name):
+    if jail_name:
+        destroy = click.confirm("Destroy {}.***REMOVED***?".format(jail_name), default=False)
+        if destroy:
+            sec_destroy = click.confirm(("[WARNING]: This will destroy ALL jail data for "
+                    "{}.***REMOVED***. ARE you sure?".format(jail_name)), default=False)
+            if sec_destroy:
+                print("destroying {} ...........".format(jail_name))
+            else:
+                sys.exit("dejailify: info: Destruction of {} was aborted.".format(jail_name))
+        else:
+            sys.exit("dejailify: info: Destruction of {} was aborted.".format(jail_name))
+    else:
+        print("other things happening.......")
 
-@root_check
-def dejailify_main():
-    print("In dejailify main")
+
