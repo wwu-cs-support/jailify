d44ebe4eab69b9b67c98c32644caf31b2e6b9aea
britney carrier <bcarrier2010@gmail.com>
2016-03-17 14:04:59 -0700

    Created a function that prompts user to delete the jail

--- jailify/__main__.py
+++ jailify/__main__.py
@@ -24,7 +24,7 @@ def root_check(func):
 @click.argument('jail_directory', type=click.Path(exists=True, readable=True))
 @root_check
 def jailify_main(jail_directory):
-    print("In jailify main and this argument thing is working {}".format(jail_directory))
+    print("In jailify main and now need to extract stuff".format(jail_directory))
 
 
 @root_check
@@ -32,17 +32,29 @@ def jailify_main(jail_directory):
 @click.argument('jail_name', required=False)
 def dejailify_main(jail_name):
     if jail_name:
-        destroy = click.confirm("Destroy {}.***REMOVED***?".format(jail_name), default=False)
-        if destroy:
-            sec_destroy = click.confirm(("[WARNING]: This will destroy ALL jail data for "
-                    "{}.***REMOVED***. ARE you sure?".format(jail_name)), default=False)
-            if sec_destroy:
-                print("destroying {} ...........".format(jail_name))
-            else:
-                sys.exit("dejailify: info: Destruction of {} was aborted.".format(jail_name))
-        else:
-            sys.exit("dejailify: info: Destruction of {} was aborted.".format(jail_name))
+        destroy_jail(jail_name)
     else:
         print("other things happening.......")
 
+##Functionized this so that way it can be used for the mass deletion of jails as
+## since they both need the same prompt patterns
+def destroy_jail(jail_name):
+    """ handles confirmation for jail destruction
 
+    Args:
+        jail_name (str): Name of jail to be destroyed
+
+    Returns:
+        None
+    """
+    destroy = click.confirm("Destroy {}.***REMOVED***?".format(jail_name), default=False)
+    if destroy:
+        sec_destroy = click.confirm(("[WARNING]: This will destroy ALL jail data for "
+                    "{}.***REMOVED***. ARE you sure?".format(jail_name)), default=False)
+        if sec_destroy:
+            print("destroying {} ...........".format(jail_name))
+        else:
+            sys.exit("dejailify: info: Destruction of {} was aborted.".format(jail_name))
+    else:
+        sys.exit("dejailify: info: Destruction of {} was aborted.".format(jail_name))
+    
